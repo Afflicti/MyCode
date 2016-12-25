5d9bccb2dda4b241936283067023153f4b6d5d14
Jakub Kopčanský <kuba.kopcansky@gmail.com>
2016-12-25 16:12:43 +0100

    ArrayPractise01
    Some new code

--- C/ArrayPractise01/ArrayPractise01/main.c
+++ C/ArrayPractise01/ArrayPractise01/main.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 
-    int Ukol1[10], x, s=0, i;
+    int Ukol1[10], x, s=0, i, Ukol2[11], pom, j;
     srand(time(NULL));
 
     /*1.	Načtěte a vytiskněte jednorozměrné pole 10 celých čísel z intervalu <1,10>.
@@ -25,6 +25,33 @@ int main(void)
      Proveďte záměnu hodnot prvků pole tak, že do
      1. prvku zapíšete hodnotu 11. prvku, do 2. prvku hodnotu 10. prvku, až do 11. prvku hodnotu 1. prvku.
      Vypište obsah pole před a po transformaci.*/
+
+    srand(time(NULL));
+
+    printf("Cisla v poli: ");
+    for(i=0;i<11;i++)
+        {
+        x = rand()%11+1;
+        Ukol2[i] = x;
+        printf("%d, ", Ukol2[i]);
+    }
+
+    i=0;
+    j=10;
+    do{
+        pom=Ukol2[i];
+        Ukol2[i]=Ukol2[j];
+        Ukol2[j]=pom;
+
+        i++;
+        j--;
+    }while(i!=5);
+
+    printf("\nCisla v poli: ");
+    for(i=0;i<11;i++)
+        {
+        printf("%d, ", Ukol2[i]);
+    }
 
 
     return 0;
